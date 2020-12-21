562fe7c95ca596cd0fdb966b59beebf9039e9184
Luis Weich <weichluis.sw@gmail.com>
2020-12-21 15:49:50 +0100

    Update

--- frontend/src/main/webapp/layouts/default.vue
+++ frontend/src/main/webapp/layouts/default.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>
-    <Nuxt />
+  <div id="default-layout">
+    <b-navbar toggleable="lg" type="dark" variant="primary">
+      <b-navbar-brand to="/">
+        {{ $t('usermanager') }}
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse" />
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto" />
+      </b-collapse>
+    </b-navbar>
+    <div id="router">
+      <Nuxt />
+    </div>
   </div>
 </template>
+
+<script></script>
 
 <style></style>
