c28ab7f8e0a57e538ce000c8041657fbe5ea24a1
Luis Weich <weichluis.sw@gmail.com>
2021-01-02 02:27:16 +0100

    Update

---
+++ frontend/src/main/webapp/components/QuickJumbotron.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="component-quick-jumbotron">
+    <b-jumbotron class="clickable" :header="header" :lead="lead">
+      <slot />
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuickJumbotron',
+  props: {
+    header: {
+      type: String,
+      required: true
+    },
+    lead: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.component-quick-jumbotron :hover {
+  background-color: lightblue;
+}
+.component-quick-jumbotron > .clickable {
+  cursor: pointer;
+}
+</style>
--- frontend/src/main/webapp/pages/index.vue
+++ frontend/src/main/webapp/pages/index.vue
@@ -1,12 +1,39 @@
 <template>
-  <div>
-    usermanager
+  <div id="usermanager-index" class="p-4">
+    <div v-if="isLoggedIn">
+      <b-row>
+        <b-col>
+          <quick-jumbotron />
+        </b-col>
+        <b-col>
+          <quick-jumbotron />
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <quick-jumbotron />
+        </b-col>
+        <b-col>
+          <quick-jumbotron />
+        </b-col>
+      </b-row>
+    </div>
   </div>
 </template>
 
 <script>
+import QuickJumbotron from '~/components/QuickJumbotron.vue'
+
 export default {
-  name: 'Index'
+  name: 'Index',
+  components: {
+    QuickJumbotron
+  },
+  mounted() {
+    if (!this.isLoggedIn) {
+      this.gotoLoginPage()
+    }
+  }
 }
 </script>
 
