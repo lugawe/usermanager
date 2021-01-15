96deb06d9f7f643396c4e96367974cabf5e83f99
Luis Weich <weichluis.sw@gmail.com>
2021-01-16 00:22:50 +0100

    Update

---
+++ backend/usermanager-service/src/test/java/com/github/lugawe/usermanager/service/h2/TestDatabase.java
@@ -0,0 +1,49 @@
+package com.github.lugawe.usermanager.service.h2;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+import org.reflections.Reflections;
+
+import javax.persistence.Entity;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public final class TestDatabase {
+
+    private TestDatabase() {
+    }
+
+    /**
+     * Builds h2 in-memory database SessionFactory
+     *
+     * @return SessionFactory object
+     * @see org.h2.Driver
+     */
+    public static SessionFactory buildSessionFactory() {
+
+        Reflections reflections = new Reflections("com.github.lugawe.usermanager.model.db");
+        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Entity.class);
+
+        Map<String, String> settings = new HashMap<>();
+        settings.put("connection.driver_class", "org.h2.Driver");
+        settings.put("hibernate.connection.url", "jdbc:h2:mem:test");
+        settings.put("hibernate.connection.username", "root");
+        settings.put("hibernate.connection.password", "password");
+        settings.put("dialect", "org.hibernate.dialect.H2Dialect");
+        settings.put("hibernate.hbm2ddl.auto", "create");
+        settings.put("hibernate.current_session_context_class", "thread");
+
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(settings).build();
+
+        MetadataSources metadataSources = new MetadataSources(serviceRegistry);
+        for (Class<?> eClass : classes) {
+            metadataSources.addAnnotatedClass(eClass);
+        }
+
+        return metadataSources.buildMetadata().getSessionFactoryBuilder().build();
+    }
+
+}
