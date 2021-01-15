55831e74ba43a80bacd25258a963e3e1dc9448f2
Luis Weich <weichluis.sw@gmail.com>
2021-01-15 23:00:09 +0100

    Update

--- backend/usermanager-service/src/main/java/com/github/lugawe/usermanager/service/db/BaseService.java
+++ backend/usermanager-service/src/main/java/com/github/lugawe/usermanager/service/db/BaseService.java
@@ -1,15 +1,20 @@
 package com.github.lugawe.usermanager.service.db;
 
 import com.github.lugawe.usermanager.db.BaseDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
 
 public abstract class BaseService<T extends BaseDAO<?>> {
 
+    private static final Logger log = LoggerFactory.getLogger(BaseService.class);
+
     private final T baseDAO;
 
     public BaseService(T baseDAO) {
         this.baseDAO = Objects.requireNonNull(baseDAO);
+        log.debug("construct {} service", baseDAO.getEntityPath());
     }
 
     public final T getBaseDAO() {
