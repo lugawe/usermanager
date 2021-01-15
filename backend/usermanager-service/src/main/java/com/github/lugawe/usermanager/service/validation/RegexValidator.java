7c110c7feb68b966d729e12aadb11a9e160a138c
Luis Weich <weichluis.sw@gmail.com>
2021-01-15 23:08:15 +0100

    Update

--- backend/usermanager-service/src/main/java/com/github/lugawe/usermanager/service/validation/RegexValidator.java
+++ backend/usermanager-service/src/main/java/com/github/lugawe/usermanager/service/validation/RegexValidator.java
@@ -6,13 +6,11 @@ import java.util.regex.Pattern;
 public class RegexValidator implements StringValidator {
 
     private final String value;
-    private final String regex;
     private final Pattern regexPattern;
 
     public RegexValidator(String value, String regex) {
-        this.value = Objects.requireNonNull(value);
-        this.regex = Objects.requireNonNull(regex);
-        this.regexPattern = Pattern.compile(this.regex);
+        this.value = value;
+        this.regexPattern = Pattern.compile(Objects.requireNonNull(regex));
     }
 
     @Override
@@ -25,15 +23,11 @@ public class RegexValidator implements StringValidator {
 
     @Override
     public boolean isValid() {
-        return regexPattern.matcher(value).matches();
+        return value != null && regexPattern.matcher(value).matches();
     }
 
     public final String getValue() {
         return value;
-    }
-
-    public final String getRegex() {
-        return regex;
     }
 
     public final Pattern getRegexPattern() {
