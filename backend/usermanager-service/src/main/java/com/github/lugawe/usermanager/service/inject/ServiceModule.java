9037fc2299d588f1d16b0033d6fb612897868d40
Luis Weich <weichluis.sw@gmail.com>
2021-01-28 20:22:05 +0100

    Update

--- backend/usermanager-service/src/main/java/com/github/lugawe/usermanager/service/inject/ServiceModule.java
+++ backend/usermanager-service/src/main/java/com/github/lugawe/usermanager/service/inject/ServiceModule.java
@@ -1,5 +1,6 @@
 package com.github.lugawe.usermanager.service.inject;
 
+import com.github.lugawe.usermanager.db.transaction.TransactionHandler;
 import com.github.lugawe.usermanager.service.config.ServiceConfig;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
@@ -14,16 +15,22 @@ public class ServiceModule extends AbstractModule {
 
     private final ServiceConfig serviceConfig;
     private final SessionFactory sessionFactory;
+    private final TransactionHandler transactionHandler;
 
-    public ServiceModule(ServiceConfig serviceConfig, SessionFactory sessionFactory) {
+    public ServiceModule(ServiceConfig serviceConfig,
+                         SessionFactory sessionFactory,
+                         TransactionHandler transactionHandler) {
+
         this.sessionFactory = Objects.requireNonNull(sessionFactory);
         this.serviceConfig = Objects.requireNonNull(serviceConfig);
+        this.transactionHandler = Objects.requireNonNull(transactionHandler);
     }
 
     @Override
     protected void configure() {
         bind(ServiceConfig.class).toInstance(serviceConfig);
         bind(SessionFactory.class).toInstance(sessionFactory);
+        bind(TransactionHandler.class).toInstance(transactionHandler);
     }
 
     public final ServiceConfig getServiceConfig() {
@@ -32,6 +39,10 @@ public class ServiceModule extends AbstractModule {
 
     public final SessionFactory getSessionFactory() {
         return sessionFactory;
+    }
+
+    public final TransactionHandler getTransactionHandler() {
+        return transactionHandler;
     }
 
     public static Injector createInjector(ServiceModule module, Module... modules) {
