a574f076e76b58c0d758b0c81f9fde06e77eac52
Luis Weich <weichluis.sw@gmail.com>
2021-01-30 17:21:21 +0100

    Update

--- backend/usermanager-db/src/main/java/com/github/lugawe/usermanager/db/transaction/TransactionHandler.java
+++ backend/usermanager-db/src/main/java/com/github/lugawe/usermanager/db/transaction/TransactionHandler.java
@@ -2,9 +2,9 @@ package com.github.lugawe.usermanager.db.transaction;
 
 public interface TransactionHandler {
 
-    public <T> T inTransaction(GenericTransaction<T> transaction);
+    public <T> T inTransaction(GenericTransaction<T> transaction) throws TransactionException;
 
-    public default void inTransaction(VoidTransaction transaction) {
+    public default void inTransaction(VoidTransaction transaction) throws TransactionException {
         if (transaction == null) {
             throw new NullPointerException("param transaction is null");
         }
