dd63e24599a21c3d1f01089a583c59dfeee2c28b
Luis Weich <weichluis.sw@gmail.com>
2021-01-30 17:20:03 +0100

    Update

--- backend/usermanager-service/src/main/java/com/github/lugawe/usermanager/service/validation/ValidationException.java
+++ backend/usermanager-service/src/main/java/com/github/lugawe/usermanager/service/validation/ValidationException.java
@@ -1,4 +1,31 @@
 package com.github.lugawe.usermanager.service.validation;
 
 public class ValidationException extends RuntimeException {
+
+    private Validator<?> validator;
+
+    public ValidationException() {
+        super();
+    }
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ValidationException(Throwable cause) {
+        super(cause);
+    }
+
+    public Validator<?> getValidator() {
+        return validator;
+    }
+
+    public void setValidator(Validator<?> validator) {
+        this.validator = validator;
+    }
+
 }
