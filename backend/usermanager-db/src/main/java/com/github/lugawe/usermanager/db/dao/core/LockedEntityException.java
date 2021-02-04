917b873b5a43a08ce90fc7c1fc2644c562ebf9f4
Luis Weich <weichluis.sw@gmail.com>
2021-02-04 19:29:44 +0100

    Update

---
+++ backend/usermanager-db/src/main/java/com/github/lugawe/usermanager/db/dao/core/LockedEntityException.java
@@ -0,0 +1,34 @@
+package com.github.lugawe.usermanager.db.dao.core;
+
+import com.github.lugawe.usermanager.model.db.base.Persistable;
+
+public class LockedEntityException extends RuntimeException {
+
+    private Persistable entity;
+
+    public LockedEntityException() {
+        super();
+    }
+
+    public LockedEntityException(String message) {
+        super(message);
+    }
+
+    public LockedEntityException(String message, Persistable entity) {
+        this(message);
+        this.entity = entity;
+    }
+
+    public LockedEntityException(Persistable entity) {
+        this("Entity is locked", entity);
+    }
+
+    public Persistable getEntity() {
+        return entity;
+    }
+
+    public void setEntity(Persistable entity) {
+        this.entity = entity;
+    }
+
+}
