53252d9d254349483698cb37cb601143802d53cf
Luis Weich <weichluis.sw@gmail.com>
2021-02-04 19:35:11 +0100

    Update

--- backend/usermanager-service/src/main/java/com/github/lugawe/usermanager/service/db/EntryService.java
+++ backend/usermanager-service/src/main/java/com/github/lugawe/usermanager/service/db/EntryService.java
@@ -2,10 +2,13 @@ package com.github.lugawe.usermanager.service.db;
 
 import com.github.lugawe.usermanager.db.dao.EntryDAO;
 import com.github.lugawe.usermanager.db.transaction.TransactionHandler;
+import com.github.lugawe.usermanager.model.db.Entry;
 import com.github.lugawe.usermanager.model.db.QEntry;
+import com.github.lugawe.usermanager.model.db.User;
 import com.github.lugawe.usermanager.service.db.core.BaseService;
 
 import javax.inject.Inject;
+import java.util.Optional;
 
 public class EntryService extends BaseService<EntryDAO> {
 
@@ -14,6 +17,19 @@ public class EntryService extends BaseService<EntryDAO> {
     @Inject
     public EntryService(EntryDAO dao, TransactionHandler handler) {
         super(dao, handler);
+    }
+
+    public Optional<Entry> getEntry(User user, String key) {
+        if (user == null) {
+            throw new NullPointerException("param user is null");
+        }
+        if (key == null || key.trim().isEmpty()) {
+            throw new IllegalArgumentException("param key is null or empty");
+        }
+        Entry result = inTransaction(() -> baseDAO.query()
+                .where(entry.user.eq(user).and(entry.key.eq(key)))
+                .fetchFirst());
+        return Optional.ofNullable(result);
     }
 
 }
