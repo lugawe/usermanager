12a6de4f72af9b5e4e96c6bfce7ae59d1bff787a
Luis Weich <weichluis.sw@gmail.com>
2021-02-06 00:26:55 +0100

    Update

---
+++ backend/usermanager-model/src/main/java/com/github/lugawe/usermanager/model/Roles.java
@@ -0,0 +1,14 @@
+package com.github.lugawe.usermanager.model;
+
+public final class Roles {
+
+    private Roles() {
+    }
+
+    public static final String ADMIN = "ADMIN";
+
+    public static final String READ = "READ";
+
+    public static final String WRITE = "WRITE";
+
+}
