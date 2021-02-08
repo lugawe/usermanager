a944bf72cf13c4e21679f948835e68dc6f7e1e07
Luis Weich <weichluis.sw@gmail.com>
2021-02-08 19:58:15 +0100

    Update

--- backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/UserManagerApp.java
+++ backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/UserManagerApp.java
@@ -1,8 +1,24 @@
 package com.github.lugawe.usermanager.server;
 
-public class UserManagerApp {
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-    public static void main(String[] args) {
+public class UserManagerApp extends Application<UserManagerConfiguration> {
+
+    private static final Logger log = LoggerFactory.getLogger(UserManagerApp.class);
+
+    private static UserManagerApp app;
+
+    public static void main(String[] args) throws Exception {
+        log.info("start");
+        app = new UserManagerApp();
+        app.run(args);
+    }
+
+    @Override
+    public void run(UserManagerConfiguration configuration, Environment environment) throws Exception {
     }
 
 }
---
+++ backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/UserManagerConfiguration.java
@@ -0,0 +1,6 @@
+package com.github.lugawe.usermanager.server;
+
+import io.dropwizard.Configuration;
+
+public class UserManagerConfiguration extends Configuration {
+}
