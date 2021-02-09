2d37731fd7ccad34578831b1f70e4232720e0b54
Luis Weich <weichluis.sw@gmail.com>
2021-02-09 19:06:18 +0100

    Update

--- backend/usermanager-service/src/main/java/com/github/lugawe/usermanager/service/config/ServiceConfig.java
+++ backend/usermanager-service/src/main/java/com/github/lugawe/usermanager/service/config/ServiceConfig.java
@@ -1,4 +1,33 @@
 package com.github.lugawe.usermanager.service.config;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.Valid;
+
 public class ServiceConfig {
+
+    @Valid
+    @JsonProperty("jwt")
+    private JwtConfig jwtConfig = new JwtConfig();
+
+    @Valid
+    @JsonProperty("validation")
+    private ValidationConfig validationConfig = new ValidationConfig();
+
+    public JwtConfig getJwtConfig() {
+        return jwtConfig;
+    }
+
+    public void setJwtConfig(JwtConfig jwtConfig) {
+        this.jwtConfig = jwtConfig;
+    }
+
+    public ValidationConfig getValidationConfig() {
+        return validationConfig;
+    }
+
+    public void setValidationConfig(ValidationConfig validationConfig) {
+        this.validationConfig = validationConfig;
+    }
+
 }
