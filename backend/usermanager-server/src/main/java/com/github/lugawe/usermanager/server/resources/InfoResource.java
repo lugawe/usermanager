13bf13d907b7ae1d9c61b50cc0e63ab84953c28a
Luis Weich <weichluis.sw@gmail.com>
2021-02-10 23:52:23 +0100

    Update

--- backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/resources/InfoResource.java
+++ backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/resources/InfoResource.java
@@ -1,21 +1,27 @@
 package com.github.lugawe.usermanager.server.resources;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.github.lugawe.usermanager.service.config.ServiceConfig;
 
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Objects;
 
 @Path("/info")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class InfoResource {
 
-    private static final Logger log = LoggerFactory.getLogger(InfoResource.class);
+    private final ServiceConfig serviceConfig;
+
+    @Inject
+    public InfoResource(ServiceConfig serviceConfig) {
+        this.serviceConfig = Objects.requireNonNull(serviceConfig);
+    }
 
     @GET
     @Path("/ping")
