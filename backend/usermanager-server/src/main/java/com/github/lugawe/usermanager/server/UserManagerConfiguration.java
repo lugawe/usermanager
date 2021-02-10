2e788ae71e70d8ffde1d9e2fe5e0a88eea9c512f
Luis Weich <weichluis.sw@gmail.com>
2021-02-10 23:06:29 +0100

    Update

--- backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/UserManagerConfiguration.java
+++ backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/UserManagerConfiguration.java
@@ -1,5 +1,6 @@
 package com.github.lugawe.usermanager.server;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.lugawe.usermanager.service.config.ServiceConfig;
 import io.dropwizard.Configuration;
 import io.dropwizard.db.DataSourceFactory;
@@ -11,9 +12,11 @@ public class UserManagerConfiguration extends Configuration {
 
     @Valid
     @NotNull
+    @JsonProperty("database")
     private DataSourceFactory database = new DataSourceFactory();
 
     @Valid
+    @JsonProperty("service")
     private ServiceConfig serviceConfig = new ServiceConfig();
 
     public DataSourceFactory getDatabase() {
