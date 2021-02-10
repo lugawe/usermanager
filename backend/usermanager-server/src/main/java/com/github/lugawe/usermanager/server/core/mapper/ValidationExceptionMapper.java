eb58d951692ac33cef86630ee776ac66503e5386
Luis Weich <weichluis.sw@gmail.com>
2021-02-10 23:20:22 +0100

    Update

---
+++ backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/core/mapper/ValidationExceptionMapper.java
@@ -0,0 +1,17 @@
+package com.github.lugawe.usermanager.server.core.mapper;
+
+import com.github.lugawe.usermanager.service.validation.ValidationException;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class ValidationExceptionMapper implements ExceptionMapper<ValidationException> {
+
+    @Override
+    public Response toResponse(ValidationException e) {
+        return Response.status(400).entity(e.getMessage()).build();
+    }
+
+}
