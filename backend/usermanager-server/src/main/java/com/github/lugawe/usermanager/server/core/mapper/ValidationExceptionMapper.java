a0a16e6e9d9d87c282bd21020a25f1f1f3c14dda
Luis Weich <weichluis.sw@gmail.com>
2021-02-10 23:35:10 +0100

    Update

--- backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/core/mapper/ValidationExceptionMapper.java
+++ backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/core/mapper/ValidationExceptionMapper.java
@@ -1,17 +1,34 @@
 package com.github.lugawe.usermanager.server.core.mapper;
 
 import com.github.lugawe.usermanager.service.validation.ValidationException;
+import com.github.lugawe.usermanager.service.validation.Validator;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+import java.util.HashMap;
+import java.util.Map;
 
 @Provider
 public class ValidationExceptionMapper implements ExceptionMapper<ValidationException> {
 
     @Override
     public Response toResponse(ValidationException e) {
-        return Response.status(400).entity(e.getMessage()).build();
+        return Response.status(400).entity(buildEntity(e)).build();
+    }
+
+    protected Map<String, String> buildEntity(ValidationException e) {
+        if (e != null) {
+            Validator<?> validator = e.getValidator();
+            if (validator != null) {
+                Map<String, String> result = new HashMap<>();
+                result.put("type", validator.type());
+                result.put("pattern", validator.pattern());
+                result.put("target", validator.target());
+                return result;
+            }
+        }
+        return null;
     }
 
 }
