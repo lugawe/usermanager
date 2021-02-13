b795b5c9989a9428b18954a4d07912b06f918e64
Luis Weich <weichluis.sw@gmail.com>
2021-02-13 16:06:28 +0100

    Update

--- backend/usermanager-service/src/main/java/com/github/lugawe/usermanager/service/db/EntryService.java
+++ backend/usermanager-service/src/main/java/com/github/lugawe/usermanager/service/db/EntryService.java
@@ -6,17 +6,48 @@ import com.github.lugawe.usermanager.model.db.Entry;
 import com.github.lugawe.usermanager.model.db.QEntry;
 import com.github.lugawe.usermanager.model.db.User;
 import com.github.lugawe.usermanager.service.db.core.BaseService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.util.Optional;
+import java.util.UUID;
 
 public class EntryService extends BaseService<EntryDAO> {
+
+    private static final Logger log = LoggerFactory.getLogger(EntryService.class);
 
     private static final QEntry entry = EntryDAO.ENTRY;
 
     @Inject
     public EntryService(EntryDAO dao, TransactionHandler handler) {
         super(dao, handler);
+    }
+
+    protected Entry create(String key, String value, User user) {
+        if (key == null || key.trim().isEmpty()) {
+            throw new IllegalArgumentException("param key is null or empty");
+        }
+        if (value == null || value.trim().isEmpty()) {
+            throw new IllegalArgumentException("param value is null or empty");
+        }
+        log.info("#create - key: {}, value: {}, user: {}", key, value, user != null ? user.getName() : "none");
+        Entry entry = new Entry();
+        entry.setKey(key);
+        entry.setValue(value);
+        entry.setUser(user);
+        return entry;
+    }
+
+    public UUID createGlobalEntry(String key, String value) {
+        return inTransaction(() -> baseDAO.insert(create(key, value, null)));
+    }
+
+    public UUID createEntry(String key, String value, User user) {
+        if (user == null) {
+            throw new NullPointerException("param user is null");
+        }
+        return inTransaction(() -> baseDAO.insert(create(key, value, user)));
     }
 
     public Optional<Entry> getGlobalEntry(String key) {
