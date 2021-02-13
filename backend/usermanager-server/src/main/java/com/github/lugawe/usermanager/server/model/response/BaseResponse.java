57ac8277ebcc3ef5d884b3dc1af9ccf1e80a11b4
Luis Weich <weichluis.sw@gmail.com>
2021-02-13 15:50:30 +0100

    Update

--- backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/model/response/BaseResponse.java
+++ backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/model/response/BaseResponse.java
@@ -1,9 +1,13 @@
 package com.github.lugawe.usermanager.server.model.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.ws.rs.core.Response;
+import java.io.Serializable;
 
-public interface BaseResponse {
+public interface BaseResponse extends Serializable {
 
+    @JsonIgnore
     Response toResponse();
 
 }
