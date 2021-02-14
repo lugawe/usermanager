a7a36fadd573d546f90d0de4c27b97132625e27e
Luis Weich <weichluis.sw@gmail.com>
2021-02-14 18:09:22 +0100

    Update

--- backend/usermanager-service/src/main/java/com/github/lugawe/usermanager/service/auth/jwt/JwtClaim.java
+++ backend/usermanager-service/src/main/java/com/github/lugawe/usermanager/service/auth/jwt/JwtClaim.java
@@ -5,6 +5,29 @@ import java.io.Serializable;
 public interface JwtClaim extends Serializable {
 
     String getKey();
+
     String getValue();
+
+    static JwtClaim of(String key, String value) {
+        if (key == null || key.trim().isEmpty()) {
+            throw new IllegalArgumentException("param key is null or empty");
+        }
+        if (value == null || value.trim().isEmpty()) {
+            throw new IllegalArgumentException("param value is null or empty");
+        }
+        return new JwtClaim() {
+
+            @Override
+            public String getKey() {
+                return key;
+            }
+
+            @Override
+            public String getValue() {
+                return value;
+            }
+
+        };
+    }
 
 }
