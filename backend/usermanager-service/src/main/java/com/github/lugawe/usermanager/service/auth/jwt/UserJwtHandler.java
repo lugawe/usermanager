6f07fa7a8da6e259731c17a85761f1ba3f3ca935
Luis Weich <weichluis.sw@gmail.com>
2021-02-16 19:04:54 +0100

    Update

--- backend/usermanager-service/src/main/java/com/github/lugawe/usermanager/service/auth/jwt/UserJwtHandler.java
+++ backend/usermanager-service/src/main/java/com/github/lugawe/usermanager/service/auth/jwt/UserJwtHandler.java
@@ -1,19 +1,27 @@
 package com.github.lugawe.usermanager.service.auth.jwt;
 
+import com.github.lugawe.usermanager.service.db.EntryService;
+
 import javax.inject.Inject;
 import java.util.Objects;
 
 public class UserJwtHandler {
 
     private final JwtHandler jwtHandler;
+    private final EntryService entryService;
 
     @Inject
-    public UserJwtHandler(JwtHandler jwtHandler) {
+    public UserJwtHandler(JwtHandler jwtHandler, EntryService entryService) {
         this.jwtHandler = Objects.requireNonNull(jwtHandler);
+        this.entryService = Objects.requireNonNull(entryService);
     }
 
     public final JwtHandler getJwtHandler() {
         return jwtHandler;
+    }
+
+    public final EntryService getEntryService() {
+        return entryService;
     }
 
 }
