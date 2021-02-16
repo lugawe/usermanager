111bae14c6bc10011f43bbce92d8c801de3d99f7
Luis Weich <weichluis.sw@gmail.com>
2021-02-16 19:08:16 +0100

    Update

--- backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/UserManagerModule.java
+++ backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/UserManagerModule.java
@@ -1,11 +1,29 @@
 package com.github.lugawe.usermanager.server;
 
+import com.github.lugawe.usermanager.server.auth.Authenticator;
+import com.github.lugawe.usermanager.server.auth.Authorizer;
+import com.github.lugawe.usermanager.service.auth.UserAuthenticator;
+import com.github.lugawe.usermanager.service.auth.UserAuthorizer;
 import com.google.inject.AbstractModule;
+
+import javax.inject.Inject;
+import java.util.Objects;
 
 public class UserManagerModule extends AbstractModule {
 
+    private final Authenticator authenticator;
+    private final Authorizer authorizer;
+
+    @Inject
+    public UserManagerModule(Authenticator authenticator, Authorizer authorizer) {
+        this.authenticator = Objects.requireNonNull(authenticator);
+        this.authorizer = Objects.requireNonNull(authorizer);
+    }
+
     @Override
     protected void configure() {
+        bind(UserAuthenticator.class).toInstance(authenticator);
+        bind(UserAuthorizer.class).toInstance(authorizer);
     }
 
 }
