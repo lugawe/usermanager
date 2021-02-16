34c57132b02671f4603cec02f0a76f31244f43c8
Luis Weich <weichluis.sw@gmail.com>
2021-02-16 19:27:16 +0100

    Update

--- backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/auth/Authorizer.java
+++ backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/auth/Authorizer.java
@@ -1,11 +1,14 @@
 package com.github.lugawe.usermanager.server.auth;
 
+import com.github.lugawe.usermanager.model.db.Role;
+import com.github.lugawe.usermanager.model.db.RoleSet;
 import com.github.lugawe.usermanager.model.db.User;
 import com.github.lugawe.usermanager.service.auth.UserAuthorizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import java.util.Set;
 
 public class Authorizer implements UserAuthorizer {
 
@@ -17,7 +20,25 @@ public class Authorizer implements UserAuthorizer {
 
     @Override
     public boolean hasAccess(User user, String role) {
-        return false;
+        if (user == null || role == null || role.isEmpty()) {
+            return false;
+        }
+        log.info("check user {} ({}) access, required role {}", user.getId(), user.getType(), role);
+        if (user.getType().equals(User.Type.ADMIN)) {
+            return true;
+        }
+        return check(user.getRoleSet(), role);
+    }
+
+    protected boolean check(RoleSet roleSet, String role) {
+        if (roleSet == null || roleSet.isLocked()) {
+            return false;
+        }
+        Set<Role> roles = roleSet.getRoles();
+        if (roles == null || roles.size() < 1) {
+            return false;
+        }
+        return roles.stream().anyMatch(r -> r != null && !r.isLocked() && r.getName().equals(role));
     }
 
 }
