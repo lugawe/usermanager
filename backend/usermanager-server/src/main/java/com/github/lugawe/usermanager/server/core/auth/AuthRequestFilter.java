a9d494fb29e4ce3cc42917dcc19ad0a3c2771388
Luis Weich <weichluis.sw@gmail.com>
2021-02-18 23:32:12 +0100

    Update

--- backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/core/auth/AuthRequestFilter.java
+++ backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/core/auth/AuthRequestFilter.java
@@ -2,14 +2,17 @@ package com.github.lugawe.usermanager.server.core.auth;
 
 import com.github.lugawe.usermanager.model.db.User;
 import io.dropwizard.auth.AuthFilter;
+import org.apache.commons.lang3.time.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Cookie;
+import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.SecurityContext;
 import java.io.IOException;
+import java.util.Date;
 import java.util.Objects;
 
 public class AuthRequestFilter extends AuthFilter<String, User> {
@@ -37,6 +40,16 @@ public class AuthRequestFilter extends AuthFilter<String, User> {
         } catch (Exception ex) {
             return "unknown";
         }
+    }
+
+    public static NewCookie createAccessTokenCookie(String token) {
+        int maxAge = 60 * 60 * 24 * 365 * 10;
+        Date expiry = DateUtils.addSeconds(new Date(), maxAge);
+        return new NewCookie(COOKIE_ACCESS_TOKEN, token,
+                null, null,
+                NewCookie.DEFAULT_VERSION, "usermanager_access_token",
+                maxAge, expiry,
+                false, true);
     }
 
     public static class Builder extends AuthFilterBuilder<String, User, AuthRequestFilter> {
