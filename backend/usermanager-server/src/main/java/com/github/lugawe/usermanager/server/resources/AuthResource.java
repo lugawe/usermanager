7d511019531467a9905a27d56b6f610292be1ead
Luis Weich <weichluis.sw@gmail.com>
2021-02-18 23:11:46 +0100

    Update

--- backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/resources/AuthResource.java
+++ backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/resources/AuthResource.java
@@ -1,12 +1,15 @@
 package com.github.lugawe.usermanager.server.resources;
 
+import com.github.lugawe.usermanager.model.db.User;
 import com.github.lugawe.usermanager.server.model.request.AuthLoginRequest;
 import com.github.lugawe.usermanager.server.model.request.AuthRegisterRequest;
 import com.github.lugawe.usermanager.server.model.response.AuthLoginResponse;
 import com.github.lugawe.usermanager.server.model.response.AuthRegisterResponse;
+import com.github.lugawe.usermanager.service.auth.jwt.UserJwtHandler;
 import com.github.lugawe.usermanager.service.config.ServiceConfig;
 import com.github.lugawe.usermanager.service.config.ValidationConfig;
 import com.github.lugawe.usermanager.service.logic.AuthService;
+import com.github.lugawe.usermanager.service.validation.Validator;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
@@ -15,6 +18,8 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Optional;
 
 @Path("/auth")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -23,23 +28,42 @@ public class AuthResource {
 
     private final ValidationConfig validationConfig;
     private final AuthService authService;
+    private final UserJwtHandler userJwtHandler;
 
     @Inject
-    public AuthResource(ServiceConfig config, AuthService authService) {
+    public AuthResource(ServiceConfig config, AuthService authService, UserJwtHandler userJwtHandler) {
         this.validationConfig = config.getValidationConfig();
         this.authService = authService;
+        this.userJwtHandler = userJwtHandler;
     }
 
     @POST
     @Path("/register")
-    public AuthRegisterResponse register(@Valid AuthRegisterRequest request) {
-        return null;
+    public Response register(@Valid AuthRegisterRequest request) {
+
+        Validator<String> name = validationConfig.userNameValidator(request.getName());
+        Validator<String> mail = validationConfig.userMailValidator(request.getMail());
+        Validator<String> password = validationConfig.userPasswordValidator(request.getPassword());
+
+        User user = authService.register(name, mail, password);
+        return new AuthRegisterResponse(true, user.getName()).toResponse();
     }
 
     @POST
     @Path("/login")
-    public AuthLoginResponse login(@Valid AuthLoginRequest request) {
-        return null;
+    public Response login(@Valid AuthLoginRequest request) {
+
+        Validator<String> name = validationConfig.userNameValidator(request.getName());
+        Validator<String> password = validationConfig.userPasswordValidator(request.getPassword());
+
+        Optional<User> user = authService.login(name, password);
+
+        if (user.isPresent()) {
+            String accessToken = userJwtHandler.encode(user.get());
+            return new AuthLoginResponse(true, null, accessToken).toResponse();
+        } else {
+            return new AuthLoginResponse(false, null, null).toResponse();
+        }
     }
 
 }
