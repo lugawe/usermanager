846f96bbeba30232c4eb3b6e2415c2400968dc49
Luis Weich <weichluis.sw@gmail.com>
2021-02-20 15:17:19 +0100

    Update

--- frontend/src/main/webapp/services/auth.js
+++ frontend/src/main/webapp/services/auth.js
@@ -1,6 +1,7 @@
 const baseURL = '/api/auth/'
 const registerURL = baseURL + 'register'
 const checkURL = baseURL + 'check'
+const logoutURL = baseURL + 'logout'
 const loginURL = baseURL + 'login'
 
 const register = function(axios, userName, userMail, userPassword) {
@@ -11,6 +12,10 @@ const check = function(axios) {
   return axios.get(checkURL)
 }
 
+const logout = function(axios) {
+  return axios.get(logoutURL)
+}
+
 const login = function(axios, userName, userPassword) {
   return axios.post(loginURL, { name: userName, password: userPassword })
 }
@@ -18,5 +23,6 @@ const login = function(axios, userName, userPassword) {
 export default {
   register,
   check,
+  logout,
   login
 }
