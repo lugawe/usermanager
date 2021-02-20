f82bffda67ebc2f725e7aaaa390887752ced49c3
Luis Weich <weichluis.sw@gmail.com>
2021-02-20 13:34:23 +0100

    Update

---
+++ backend/usermanager-server/src/main/java/com/github/lugawe/usermanager/server/util/CookieBuilder.java
@@ -0,0 +1,76 @@
+package com.github.lugawe.usermanager.server.util;
+
+import javax.ws.rs.core.NewCookie;
+import java.util.Date;
+
+public class CookieBuilder {
+
+    private String name;
+    private String value;
+    private String path;
+    private String domain;
+    private int version = NewCookie.DEFAULT_VERSION;
+    private String comment;
+    private int maxAge = NewCookie.DEFAULT_MAX_AGE;
+    private Date expiry;
+    private boolean secure;
+    private boolean httpOnly;
+
+    public CookieBuilder() {
+    }
+
+    public CookieBuilder name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public CookieBuilder value(String value) {
+        this.value = value;
+        return this;
+    }
+
+    public CookieBuilder path(String path) {
+        this.path = path;
+        return this;
+    }
+
+    public CookieBuilder domain(String domain) {
+        this.domain = domain;
+        return this;
+    }
+
+    public CookieBuilder version(int version) {
+        this.version = version;
+        return this;
+    }
+
+    public CookieBuilder comment(String comment) {
+        this.comment = comment;
+        return this;
+    }
+
+    public CookieBuilder maxAge(int maxAge) {
+        this.maxAge = maxAge;
+        return this;
+    }
+
+    public CookieBuilder expiry(Date expiry) {
+        this.expiry = expiry;
+        return this;
+    }
+
+    public CookieBuilder secure(boolean secure) {
+        this.secure = secure;
+        return this;
+    }
+
+    public CookieBuilder httpOnly(boolean httpOnly) {
+        this.httpOnly = httpOnly;
+        return this;
+    }
+
+    public NewCookie build() {
+        return new NewCookie(name, value, path, domain, version, comment, maxAge, expiry, secure, httpOnly);
+    }
+
+}
