1ce1a8a9b86726fddaa3087f6700a9e03a799a7c
Luis Weich <weichluis.sw@gmail.com>
2021-02-20 17:55:50 +0100

    Update

--- frontend/src/main/webapp/store/auth.js
+++ frontend/src/main/webapp/store/auth.js
@@ -1,32 +1,17 @@
 export const state = () => ({
   user: {},
-  token: {
-    access: null,
-    refresh: null
-  },
   isLoggedIn: false
 })
 
 export const mutations = {
   logout(state) {
     state.user = {}
-    state.token = {
-      access: null,
-      refresh: null
-    }
     state.isLoggedIn = false
   },
   login(state, data) {
     if (data) {
       state.user = data.user
-      state.token = data.token
       state.isLoggedIn = true
     }
-  },
-  accessToken(state, token) {
-    state.token.access = token
-  },
-  refreshToken(state, token) {
-    state.token.refresh = token
   }
 }
